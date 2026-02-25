353b4e50dcc4aedeb3cc6584ee82176753a2db7e
Tom Nielsen <tanielsen@gmail.com>
2026-02-25 15:29:54 +0000

    audio transcribe from llm

---
+++ index.js
@@ -0,0 +1,62 @@
+const Workflow = require("@saltcorn/data/models/workflow");
+const Form = require("@saltcorn/data/models/form");
+const File = require("@saltcorn/data/models/file");
+const User = require("@saltcorn/data/models/user");
+const FieldRepeat = require("@saltcorn/data/models/fieldrepeat");
+const Plugin = require("@saltcorn/data/models/plugin");
+const { domReady } = require("@saltcorn/markup/tags");
+const db = require("@saltcorn/data/db");
+
+const configuration_workflow = () =>
+  new Workflow({
+    steps: [
+      {
+        name: "API key",
+        form: async (context) => {
+          return new Form({
+            fields: [
+              {
+                name: "api_key",
+                label: "API key",
+                sublabel: "ElevenLabs API key",
+                type: "String",
+                required: true,
+                fieldview: "password",
+              },
+            ],
+          });
+        },
+      },
+    ],
+  });
+
+const functions = (config) => {
+  return {
+    llm_transcribe: {
+      run: async (opts) => {
+        return await new ElevenLabsClient({
+          apiKey: opts?.api_key || config.api_key,
+        }).speechToText.convert({
+          file: await (await File.findOne(opts.file)).get_contents(),
+          modelId: opts.model || "scribe_v2", // Model to use
+          tagAudioEvents: true, // Tag audio events like laughter, applause, etc.
+          languageCode: opts.languageCode || "eng", // Language of the audio file. If set to null, the model will detect the language automatically.
+          numSpeakers: opts.numSpeakers || null, // Language of the audio file. If set to null, the model will detect the language automatically.
+          diarize: !!opts.diarize, // Whether to annotate who is speaking
+          diarizationThreshold: opts.diarizationThreshold || null,
+        });
+      },
+      isAsync: true,
+      description: "Get vector embedding",
+      arguments: [
+        { name: "options", type: "JSON", tstype: "any", required: true },
+      ],
+    },
+  };
+};
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  configuration_workflow,
+  functions,
+};
